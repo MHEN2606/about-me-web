bc601efb9b09681e16c768dcc5b8d71c8732b9d4
Matthew Mahendra <matthew.mahendra@gmail.com>
2021-12-31 00:57:57 +0700

    Edited the content

--- src/about-me.js
+++ src/about-me.js
@@ -1,4 +1,4 @@
-import logo from './logo-matt.png';
+import picture from './matthew.jpg';
 const AboutMe = () =>{
     const title = "About Me";
     const age = 2021 - 2003
@@ -9,11 +9,21 @@ const AboutMe = () =>{
 
         <div className="column">
             <div>
-                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQG6Iacmk5bpAQ/profile-displayphoto-shrink_200_200/0/1632934624732?e=1645056000&v=beta&t=8tzJ_Ydgxh6oPRANvW5KZjs4T1Hu9pLA0gptx-M0t50" alt='logo matthew'></img>
+                <img src={picture} alt='matthew-mahendra' width="400px" heigth="700px"></img>
             </div>
 
             <div>
-                Matthew Mahendra was born in Jakarta at 26th of June 2003, he is currently {age} years old. A Computer Science/Informatics student at Bandung Institute of Technology, he was admitted in 2021 as an undergraduate student. Currently, he is interested in ReactJS and Python in web development.
+                <p>
+                    An enthusiastic student who thinks he is the Duke of Wellington because of a hooky nose. Hi I'm Matthew Mahendra. I'm an undergraduate
+                    student majoring in Informatics at Bandung Institute of Technology, Jatinangor Campus. I'm currently learning about ReactJS and this web
+                    was also created with ReactJS. So Informatics (or computer science if you're an American) to me is quite important because it shapes the future
+                    I mean, what kind of work could get done in the 21st century, even my campus started online learning due to COVID. I also have a fond with computers
+                    since I was a toddler when my parents showed me a desktop computer. It was fun and I think the path to my future has been written that day.
+                </p>
+                <p>
+                    Besides web-developing, I can also script using LaTeX, do something with Python or Jupyter Notebook, and singing. Yes singing is my leisure activity. I once joined
+                    a choir group for 6 years straight. Well I hope you enjoy this website. Contact me if you have any opinion on this website.
+                </p>
             </div>
         </div>
         </>
