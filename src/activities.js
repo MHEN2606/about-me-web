b5dabcbd8db814e35747a81b69d231d9a3cf2919
Matthew Mahendra <matthew.mahendra@gmail.com>
2021-12-30 10:33:44 +0700

    Added Activities Page

---
+++ src/activities.js
@@ -0,0 +1,20 @@
+const Activities = ({activities}) => {
+    return(
+        <>
+        <h1>Activities</h1>
+        <p>Matthew Mahendra's activities</p>
+        {activities.map((activities)=>(
+            <div className="column" key={activities.id}>
+                <div>
+                    <h3>{activities.name}</h3>
+                    <p>Position: {activities.position}</p>
+                    <p>Years Active: {activities.yearsActive}</p>
+                </div>
+                
+            </div>
+        ))}
+        </>
+    );
+}
+
+export default Activities
